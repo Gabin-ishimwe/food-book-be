fc1783ccb6d8b0de71e2905df49d4dbbc4263b8e
gabin <g.ishimwe@alustudent.com>
2022-10-28 22:43:36 +0200

    added jasypt

--- src/main/java/com/backend/app/foodbook/FoodBookBeApplication.java
+++ src/main/java/com/backend/app/foodbook/FoodBookBeApplication.java
@@ -1,11 +1,13 @@
 package com.backend.app.foodbook;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEncryptableProperties
 public class FoodBookBeApplication {
 
     public static void main(String[] args) {
